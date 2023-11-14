7ba5e73adcde437eaa7362a9c24f616723bd7834
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-11-14 19:08:46 +0700

    membuat variabel Txt

--- app/src/main/java/com/example/navdata/ui/FormHalaman.kt
+++ app/src/main/java/com/example/navdata/ui/FormHalaman.kt
@@ -1,8 +1,10 @@
 package com.example.navdata.ui
 
 import androidx.compose.runtime.Composable
-
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 
 @Composable
@@ -10,5 +12,13 @@ fun FormHalaman(
     onCancelButtonClicked: () -> Unit,
     onSubmitButtonClicked: (MutableList<String>)-> Unit
 ){
-
+    var namaTxt by remember {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var tlpnTxt by remember {
+        mutableStateOf("")
+    }
 }
