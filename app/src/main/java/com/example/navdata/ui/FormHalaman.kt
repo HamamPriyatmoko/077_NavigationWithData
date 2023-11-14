c6a77b3704545734440b7cc04128ff1ac1476ea5
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-11-14 19:24:22 +0700

    menambahkan row

--- app/src/main/java/com/example/navdata/ui/FormHalaman.kt
+++ app/src/main/java/com/example/navdata/ui/FormHalaman.kt
@@ -2,15 +2,15 @@ package com.example.navdata.ui
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-
 import androidx.compose.material3.ExperimentalMaterial3Api
-
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,7 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.navdata.R
@@ -66,9 +66,20 @@ fun FormHalaman(
         )
         Spacer(modifier = Modifier.height(15.dp))
 
-        Button(onClick = { onSubmitButtonClicked(listData) }) {
-            Text(text = stringResource(id = R.string.Submit))
+        Row (modifier = Modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_medium))
+            .weight(1f, false),
+            horizontalArrangement =
+            Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom){
+            OutlinedButton(modifier = Modifier.weight(1f), onClick =
+            onCancelButtonClicked) {
+                Text(stringResource(R.string.Batal))
+            }
+            Button(modifier = Modifier.weight(1f), onClick = { onSubmitButtonClicked(listData) }) {
+                Text(text = stringResource(id = R.string.Submit))
+            }
         }
-
     }
 }
