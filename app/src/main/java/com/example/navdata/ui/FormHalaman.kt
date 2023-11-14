c06c69db84e3a44c0d250f236147c14046f8e3c6
HamamPriyatmoko <hamampriyatmoko81@gmail.com>
2023-11-14 19:05:56 +0700

    Membuat parameter unit button

--- app/src/main/java/com/example/navdata/ui/FormHalaman.kt
+++ app/src/main/java/com/example/navdata/ui/FormHalaman.kt
@@ -1,2 +1,14 @@
 package com.example.navdata.ui
 
+import androidx.compose.runtime.Composable
+
+
+
+
+@Composable
+fun FormHalaman(
+    onCancelButtonClicked: () -> Unit,
+    onSubmitButtonClicked: (MutableList<String>)-> Unit
+){
+
+}
